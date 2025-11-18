b5e1641e13e79ae3e9595fc5a2ffaf2755d760d4
priyanksharma22 <priyank22022006@gmail.com>
2025-11-18 08:56:50 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -5,7 +5,6 @@ const userSchema = new mongoose.Schema(
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    role: { type: String, enum: ["user", "admin"], default: "user" },
   },
   { timestamps: true }
 );
