1440040498b18e59ec0796dc89eed7052d08f8f6
priyanksharma22 <priyank22022006@gmail.com>
2025-11-18 08:58:56 +0530

    Update roomRoutes.js

--- routes/roomRoutes.js
+++ routes/roomRoutes.js
@@ -1,11 +1,10 @@
 import express from "express";
-import { getAllRooms, createRoom } from "../controllers/roomController.js";
+import { getAllRooms } from "../controllers/roomController.js";
 import authMiddleware from "../middleware/authMiddleware.js";
-import adminMiddleware from "../middleware/adminMiddleware.js";
 
 const router = express.Router();
 
+// @route   GET api/rooms
 router.get("/", authMiddleware, getAllRooms);
-router.post("/", authMiddleware, adminMiddleware, createRoom);
 
 export default router;
