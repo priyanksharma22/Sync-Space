d38a8535d9c5f766173edfdbf27e3a84a3acc083
priyanksharma22 <priyank22022006@gmail.com>
2025-11-18 08:56:16 +0530

    Update Booking.js

--- models/Booking.js
+++ models/Booking.js
@@ -2,18 +2,21 @@ import mongoose from "mongoose";
 
 const bookingSchema = new mongoose.Schema(
   {
-    userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-    roomId: { type: mongoose.Schema.Types.ObjectId, ref: "Room", required: true },
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    roomId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Room",
+      required: true,
+    },
     date: { type: Date, required: true },
     startTime: { type: String, required: true },
     endTime: { type: String, required: true },
-    status: {
-      type: String,
-      enum: ["pending", "approved", "rejected"],
-      default: "pending",
-    },
   },
-  { timestamps: true } // automatically add two fields :- createdAt , updatedAt and stores them
+  { timestamps: true }
 );
 
 export default mongoose.model("Booking", bookingSchema);
